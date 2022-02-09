8c796c2fd6fd309389ed494fb034cb45075057a6
sg7275 <sg7275@nyu.edu>
2022-02-09 12:49:04 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -82,6 +82,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # print(recv7)
     # if recv7[:3] != '250':
     #     print('250 reply not received from server.')
+    
+    pass
     # Fill in end
 
 
